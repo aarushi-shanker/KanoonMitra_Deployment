f933c942fae7d93f5bceb8935548eae6dbbc0ad4
aarushi-shanker <aarushishanker810@gmail.com>
2025-07-18 05:35:41 +0530

    Add dependency for client

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.use('/api/v1/lawyer', lawyerRouter);
 app.use('/api/v1/model', modelRouter);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, '..', './client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'));
 });
 
 //port
