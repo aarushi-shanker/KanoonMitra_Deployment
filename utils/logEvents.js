0f332ecd2822fa74eccf0f2f7143f92af5baeec3
aarushi-shanker <aarushishanker810@gmail.com>
2025-08-01 03:12:31 +0530

    Logging correction

--- utils/logEvents.js
+++ utils/logEvents.js
@@ -2,7 +2,7 @@ import Log from "../models/logModel.js";
 
 const logEvent = async (eventType, details = {}) => {
   try {
-    await EventModel.create({ eventType, details });
+    await Log.create({ eventType, details });
   } catch (err) {
     console.error("Event logging failed:", err.message);
   }
